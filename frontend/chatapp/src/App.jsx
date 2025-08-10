07bf60d4146695116085897181fae5b0f7967604
pneet <student@gamil.com>
2025-08-10 20:47:53 +0530

    added to git

---
+++ frontend/chatapp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-blue-900 justify-center p-56'>React app</h1>
+    </>
+  )
+}
+
+export default App
